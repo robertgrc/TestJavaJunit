bf25b28e28a4ca1481126133177f7a29b4dee79d
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-09-07 19:47:18 -0400

    Enviando variables desde feature files

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.LoginPage;
 import utilities.DriverManager;
@@ -7,17 +8,22 @@ import utilities.DriverManager;
 public class LoginSteps {
 
     private LoginPage loginPage = new LoginPage(DriverManager.getInstance().getDriver());
-    @When("I set the user name text box with a valid user name")
-    public void setUserName(){
-        loginPage.setUserNameTextBox("standard_user");
+    @When("I set the user name text box with {string}")
+    public void setUserName(String userName){
+        loginPage.setUserNameTextBox(userName);
     }
-    @When("I set the password text box with a valid password")
-    public void setPassword(){
-        loginPage.setPasswordTextBox("secret_sauce");
+    @When("I set the password text box with {string}")
+    public void setPassword(String password){
+        loginPage.setPasswordTextBox(password);
     }
 
     @When("I click on the login button")
     public void clickOnLoginButton(){
         loginPage.clickOnLoginButton();
+    }
+
+    @Then("I verify that a error message that says {string} is displayed")
+    public void verifyErrorMessageText(String errorText){
+        loginPage.verifyErrorMessageText(errorText);
     }
 }
--- target/test-classes/Login.feature
+++ target/test-classes/Login.feature
@@ -2,7 +2,15 @@ Feature: As a user to be able to login into Sauce Labs Demo Web Page
 
   Scenario: Login into Sauce Demo page with valid credentials
     Given I am in the Sauce Labs Demo Page
-    When I set the user name text box with a valid user name
-    And I set the password text box with a valid password
+    When I set the user name text box with "standard_user"
+    And I set the password text box with "secret_sauce"
     And I click on the login button
     Then I should be in the home page
+
+
+    Scenario:Login into Sauce Demo page with invalid credentials
+      Given I am in the Sauce Labs Demo Page
+      When I set the user name text box with "wrong_user"
+      And I set the password text box with "wrong_password"
+      And I click on the login button
+      Then I verify that a error message that says "Epic sadface: Username and password do not match any user in this service" is displayed
